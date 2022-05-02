26d13bb5dc7dad187ed4e98980f79d7abe622339
Ryan MacLeod <rkmacleod17@gmail.com>
2022-05-02 12:31:22 -0400

    Dice Class v1
    Added get, set, and roll functions, overloaded constructors, and a main.cpp test file

---
+++ Dice.cpp
@@ -0,0 +1,102 @@
+#include <cstdlib>
+#include <ctime>
+#include "Dice.h"
+
+//Default Constructor #0
+Dice::Dice()
+{
+    sides = 6;
+    num = 1;
+    mod = 0;
+}
+
+//Constructor #1
+Dice::Dice(int s)
+{
+    //Initial values
+    sides = s;
+    num = 1;
+    mod = 0;
+
+    //Initialize random seed
+    srand(time(NULL));
+
+    //Generate roll value
+    roll();
+}
+
+//Constructor #2
+Dice::Dice(int s, int n)
+{
+    //Initial values
+    sides = s;
+    num = n;
+    mod = 0;
+
+    //Initialize random seed
+    srand(time(NULL));
+
+    //Generate roll value
+    roll();
+}
+
+//Constructor #3
+Dice::Dice(int s, int n, int m)
+{
+    //Initial values
+    sides = s;
+    num = n;
+    mod = m;
+
+    //Initialize random seed
+    srand(time(NULL));
+
+    //Generate roll value
+    roll();
+}
+
+void Dice::roll()
+{
+    int sum = 0;    //for adding dice rolls
+
+    //Generate value for dice rolls
+    for(int i = 0; i < num; i++)
+    {
+        sum += (rand() % sides + MIN_SIDES);
+    }
+
+    //Add rolls and modifiers to value
+    val = sum + mod;
+}
+
+void Dice::setSides(int s)
+{
+    if(s >= 1)
+        sides = s;
+}
+
+void Dice::setNumber(int n)
+{
+    if(n >= 1)
+        num = n;
+}
+
+void Dice::setModifier(int m)
+{
+    mod = m;
+}
+
+int Dice::getSides()
+{
+    return sides;
+}
+
+int Dice::getNumber()
+{
+    return num;
+}
+
+int Dice::getModifier()
+{
+    return mod;
+}
---
+++ Dice.h
@@ -0,0 +1,28 @@
+#ifndef DICE_H
+#define DICE_H
+
+class Dice
+{
+    private:
+        int sides;                  //Number of sides on dice
+        int num;                    //Number of dice rolled
+        int mod;                    //Modifiers that add/sub to overall value
+        int val;                    //The die's value when rolled
+        const int MIN_SIDES = 1;    //Constant for minimum value of dice sides
+
+    public:
+        Dice();
+        Dice(int);
+        Dice(int, int);
+        Dice(int, int, int);
+        void roll();
+        void setSides(int);
+        void setNumber(int);
+        void setModifier(int);
+        int getSides();
+        int getNumber();
+        int getModifier();
+        int getValue();
+};
+
+#endif
